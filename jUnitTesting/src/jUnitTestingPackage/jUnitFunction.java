6835cf81038c8b1cba25616b621284d00c29e06a
AADI JAIN <jainaadi7102@gmail.com>
2022-04-19 15:39:41 +0530

    Add files via upload

---
+++ jUnitTesting/src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,26 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	public int AddNumbers(int a, int b){
+		int sum = a + b;
+		return sum;
+	}
+	
+	public String AddStrings(String a, String b){
+		String f = a + b;
+		return f;
+	}
+	
+	public static void main(String[] args) {
+//		int num1 = 25;
+//		int num2 = 12;
+//		
+//		jUnitFunction obj = new jUnitFunction();
+//		int result = obj.AddNumbers(num1, num2);
+//		String word = obj.AddStrings("Aadi", "Jain");
+//		
+//		System.out.println("The Sum is: "+ result);
+//		
+//		System.out.println("The final word is: "+ word);
+	}
+}
